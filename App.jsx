5563d39b78073c70e1eb8fc545fe4f7bf23f5c64
Koki Takahashi <hakatasiloving@gmail.com>
2018-05-19 01:41:36 +0900

    Temporal

---
+++ App.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+module.exports = class extends React.Component {
+	render() {
+		return (
+			<div>Hello, World!</div>
+		);
+	}
+};
--- index.babel.js
+++ index.babel.js
@@ -1,1 +1,7 @@
-console.log('hello!');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const App = require('./App.jsx');
+
+const app = document.getElementById('app');
+ReactDOM.render(React.createElement(App), app);
