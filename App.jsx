3122658bcb0ac46669425355692ca37f359c4b64
Koki Takahashi <hakatasiloving@gmail.com>
2018-05-19 10:16:03 +0900

    Checkpoint commit to deploy for TSG LIVE! viewers

--- App.jsx
+++ App.jsx
@@ -1,7 +1,27 @@
 const React = require('react');
 
 module.exports = class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			blocks: [{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}],
+		};
+	}
+
 	render() {
-		return <div>hoge</div>;
+		return (
+			<svg>
+				{this.state.blocks.map((block, index) => (
+					<rect
+						key={index}
+						x={block.x * 10}
+						y={block.y * 10}
+						width="10"
+						height="10"
+						fill="red"
+					/>
+				))}
+			</svg>
+		);
 	}
 };
