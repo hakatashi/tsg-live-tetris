056d96d15a05bb047f5e462cb949a6cbc9236b3a
Koki Takahashi <hakatasiloving@gmail.com>
2018-05-19 10:25:35 +0900

    Checkpoint commit to deploy for TSG LIVE! viewers

--- App.jsx
+++ App.jsx
@@ -1,16 +1,23 @@
 const React = require('react');
 
+const data = require('./data.json');
+
+require('./index.pcss');
+
 module.exports = class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			blocks: [{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}],
+			blocks: [],
+			blockX: 50,
+			blockY: 30,
+			blockType: 'Z',
 		};
 	}
 
 	render() {
 		return (
-			<svg>
+			<svg width="100%" height="100%" viewBox="0 0 100 200">
 				{this.state.blocks.map((block, index) => (
 					<rect
 						key={index}
@@ -18,9 +25,20 @@ module.exports = class App extends React.Component {
 						y={block.y * 10}
 						width="10"
 						height="10"
-						fill="red"
+						fill={['red', 'blue', 'yellow'][block.type]}
 					/>
 				))}
+				{this.state.blockType && (
+					<path
+						d={`M ${data.minos[this.state.blockType].vertices
+							.map(({x, y}) => `${x} ${y}`)
+							.join(' L ')} Z`}
+						fill={data.minos[this.state.blockType].color}
+						transform={`translate(${this.state.blockX}, ${
+							this.state.blockY
+						})`}
+					/>
+				)}
 			</svg>
 		);
 	}
